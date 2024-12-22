f1707de0945196a85d36bb368dcecfef4996ad52
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2024-12-22 13:31:17 -0300

    feat: add admin dashboard with recent appointment statistics and stat card component

--- src/actions/appointment.actions.ts
+++ src/actions/appointment.actions.ts
@@ -1,9 +1,11 @@
 'use server'
 
-import { ID } from 'node-appwrite'
+import { ID, Query } from 'node-appwrite'
 
 import { databases, getEnvVariables } from '@/lib/appwrite.config'
 import { parseStringify } from '@/lib/utils'
+
+import { Appointment } from '../../types/appwrite.types'
 
 const { databaseId, appointmentCollectionId } = getEnvVariables()
 
@@ -37,5 +39,46 @@ export const getAppointment = async (appointmentId: string) => {
     return parseStringify(appointment)
   } catch (error) {
     console.error('error getAppointment', error)
+  }
+}
+
+export const getRecentAppointmentsList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      databaseId!,
+      appointmentCollectionId!,
+      [Query.orderDesc('$createdAt')],
+    )
+
+    const initialCounts = {
+      scheduleCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    }
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if (appointment.status === 'scheduled') {
+          acc.scheduleCount += 1
+        } else if (appointment.status === 'pending') {
+          acc.pendingCount += 1
+        } else if (appointment.status === 'cancelled') {
+          acc.cancelledCount += 1
+        }
+
+        return acc
+      },
+      initialCounts,
+    )
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    }
+
+    return parseStringify(data)
+  } catch (error) {
+    console.error('error getRecentAppointmentsList', error)
   }
 }
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { getRecentAppointmentsList } from '@/actions/appointment.actions'
+import { StatCard } from '@/components'
+
+export default async function Admin() {
+  const appointments = await getRecentAppointmentsList()
+
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/admin" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={162}
+            height={32}
+            className="h-8 w-fit"
+          />
+        </Link>
+
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Bem vindo 👋🏻</h1>
+          <p className="text-dark-700">
+            Comece o dia com o gerenciamento de novos agendamentos!
+          </p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={appointments.scheduleCount}
+            label="Consultas agendadas"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Consultas pendentes"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Consultas canceladas"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+      </main>
+    </div>
+  )
+}
